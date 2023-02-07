952b3bac1d40d346fc8dd8c752920684c9390682
ivarbelis <belis@oksystem.cz>
2023-02-07 11:32:27 +0100

    test2

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,7 +11,7 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
-// triggering a build
+// triggering a build - next build
 
 public class RxEndpoints {
    public static void initalizeEndpoints() {
